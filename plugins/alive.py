3b5b2e1d7b715c6865448d1ccf03199a8b567776
NOBITA_XD <101139721+NOBITAXDD@users.noreply.github.com>
2022-03-26 11:16:51 +0530

    Update alive.py

--- plugins/alive.py
+++ plugins/alive.py
@@ -33,19 +33,19 @@ async def _human_time_duration(seconds):
 @Client.on_message(command("start") & filters.private & ~filters.edited)
 async def start_(client: Client, message: Message):
     await message.reply_photo(
-        photo=f"https://te.legra.ph/file/1a85ce935e13e92706907.jpg",
+        photo=f"https://te.legra.ph/file/ea0a49d9aed8ed552b83f.jpg",
         caption=f"""**━━━━━━━━━━━━━━━━━━━━━━━━
 💥 ʜᴇʟʟᴏ, ɪ ᴀᴍ ɴᴏʙɪᴛᴀ sᴜᴘᴇʀ ғᴀsᴛ ᴠᴄ ᴘʟᴀʏᴇʀ
 ʙᴏᴛ ғᴏʀ ᴛᴇʟᴇɢʀᴀᴍ ɢʀᴏᴜᴘs ...
 ┏━━━━━━━━━━━━━━━━━┓
 ┣★ ᴄʀᴇᴀᴛᴏʀ : [KULDIP RATHOD](https://t.me/AAPLI_YAARI)
-┣★ ᴜᴘᴅᴀᴛᴇs : [NOBITA SUPPORT](https://t.me/Nobita_shizuka_repo)
+┣★ ᴜᴘᴅᴀᴛᴇs : [NOBITA SUPPORT](https://t.me/NOBITA_SPAMMER_OP)
 ┣★ sᴜᴘᴘᴏʀᴛ : [NOBITA CHAT](https://t.me/AAPLI_YAARI)
-┣★ ᴏᴡɴᴇʀ › : [NOBITA_XD](@Nobi_xxd)
+┣★ ᴏᴡɴᴇʀ › : [NOBITA_XD](https://t.me/Nobi_xxd)
 ┗━━━━━━━━━━━━━━━━━┛
 
-💞 ɪғ ʏᴏᴜ ʜᴀᴠᴇ ᴀɴʏ ǫᴜᴇsᴛɪᴏɴs ᴛʜᴇɴ
-ᴅᴍ ᴛᴏ ᴍʏ [ʟᴇɢᴇɴᴅ ᴏᴡɴᴇʀ](https://t.me/AAPLI_YAARI) ...
+😈 ɪғ ʏᴏᴜ ʜᴀᴠᴇ ᴀɴʏ ǫᴜᴇsᴛɪᴏɴs ᴛʜᴇɴ
+ᴅᴍ ᴛᴏ ᴍʏ [ʟᴇɢᴇɴᴅ ᴏᴡɴᴇʀ](https://t.me/Nobi_xxd) ...
 ━━━━━━━━━━━━━━━━━━━━━━━━**""",
     reply_markup=InlineKeyboardMarkup(
             [
@@ -62,13 +62,13 @@ async def start_(client: Client, message: Message):
 @Client.on_message(commandpro(["/start", "/alive", "NOBITA"]) & filters.group & ~filters.edited)
 async def start(client: Client, message: Message):
     await message.reply_photo(
-        photo=f"https://te.legra.ph/file/1a85ce935e13e92706907.jpg",
+        photo=f"https://te.legra.ph/file/ea0a49d9aed8ed552b83f.jpg",
         caption=f"""""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "💥 ᴊᴏɪɴ ʜᴇʀᴇ ᴀɴᴅ sᴜᴘᴘᴏʀᴛ 💞", url=f"https://t.me/AAPLI_YAARI")
+                        "❤️ 𝙹𝙾𝙸𝙽 𝙷𝙴𝚁𝙴 𝙰𝙽𝙳 𝚂𝚄𝙿𝙿𝙾𝚁𝚃 ", url=f"https://t.me/AAPLI_YAARI")
                 ]
             ]
         ),
@@ -78,13 +78,13 @@ async def start(client: Client, message: Message):
 @Client.on_message(commandpro(["repo", "#repo", "@repo", "/repo", "source"]) & filters.group & ~filters.edited)
 async def help(client: Client, message: Message):
     await message.reply_photo(
-        photo=f"https://te.legra.ph/file/1a85ce935e13e92706907.jpg",
+        photo=f"https://te.legra.ph/file/ea0a49d9aed8ed552b83f.jpg",
         caption=f"""""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "💥 ᴄʟɪᴄᴋ ᴍᴇ ᴛᴏ ɢᴇᴛ ʀᴇᴘᴏ 💞", url=f"https://github.com/kuldiprathod/JAANMUSIC")
+                        "𝙶𝙴𝚃 𝚁𝙴𝙿𝙾 𝚃𝙾 𝙲𝙾𝙽𝚃𝙰𝙲𝚃 𝙼𝚈 𝙾𝚆𝙽𝙴𝚁 ", url=f"https://t.me/Nobi_xxd")
                 ]
             ]
         ),
